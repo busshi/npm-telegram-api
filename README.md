268bf6b89e29e72733abc8a4ed3288f45813a8a9
busshi <busshi973@icloud.com>
2022-12-12 18:07:55 +0100

    add new functionnalities

--- README.md
+++ README.md
@@ -11,16 +11,37 @@ Node package for the Telegram API
 ## Functionnalities
 
 - sendMessage with/without notification
-- sendPhoto with a given URL
+- editMessage
+- deleteMessage
+- sendPhoto (with a picture URL)
+- sendDocument (with a document URL)
+- sendVideoNote (with a video URL)
+- sendPoll (question, array of answers)
+- sendSticker (sticker_id needed, for example CAACAgIAAxkBAAI6GV6vP_F6BaWapEmBZ2k8e8yBi2xMAAICAwACusCVBTRFBuRNlNodGQQ)
+- sendDice (random number from 1 to 6)
 
 ## Example
 
 ```js
-import Telegram from '@busshi/telegram-api'
+import Telegram from '@busshi/telegram-api';
 
-const telegram = new Telegram(YOUR_BOT_TOKEN');
+const telegram = new Telegram(BOT_TOKEN);
 
-telegram.sendMessage(YOUR_CHAT_ID, "Hello world!")
+const req = await telegram.sendMessage(CHAT_ID, 'Hello world!');
+const messageId = req.dat.result.message_id;
+telegram.editMessage(CHAT_ID, messageId, 'Edited message');
+telegram.deleteMessage(CHAT_ID, messageId);
 
-telegram.sendPhoto(YOUR_CHAT_ID, PICTURE_URL)
+telegram.sendPhoto(CHAT_ID, PICTURE_URL);
+
+telegram.sendDocument(CHAT_ID, DOCUMENT_URL);
+
+telegram.sendVideoNote(CHAT_ID, VIDEO_URL);
+
+telegram.sendPoll(CHAT_ID, QUESTION, OPTIONS);
+telegram.sendPoll(CHAT_ID, 'Is it working?', ['yes', 'no']);
+
+telegram.sendSticker(CHAT_ID, STICKER_ID);
+
+telegram.sendDice(CHAT_ID);
 ```
--- src/Telegram.ts
+++ src/Telegram.ts
@@ -9,15 +9,15 @@ class Telegram {
     this.apiUrl = `${this.telegramUrl}bot${token}`;
   }
 
-  sendMessage = async (
-    dest: string,
+  sendMessage = (
+    chatId: string,
     message: string,
-    disableNotification = true
+    disableNotification = false
   ) => {
-    await axios.post(
+    return axios.post(
       `${this.apiUrl}/sendMessage`,
       {
-        chat_id: dest,
+        chat_id: chatId,
         text: message,
         disable_notification: disableNotification,
       },
@@ -25,17 +25,115 @@ class Telegram {
     );
   };
 
-  sendPhoto = async (
-    dest: string,
-    picUrl: string,
-    disableNotification = true
-  ) => {
-    await axios.post(
+  sendPhoto = (chatId: string, picUrl: string, disableNotification = false) => {
+    return axios.post(
       `${this.apiUrl}/sendPhoto`,
       {
-        chat_id: dest,
+        chat_id: chatId,
         photo: picUrl,
         disable_notification: disableNotification,
+      },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+  };
+
+  sendVideoNote = (
+    chatId: string,
+    videoUrl: string,
+    disableNotification = false
+  ) => {
+    return axios.post(
+      `${this.apiUrl}/sendVideoNote`,
+      {
+        chat_id: chatId,
+        video_note: videoUrl,
+        disable_notification: disableNotification,
+      },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+  };
+
+  sendDocument = (
+    chatId: string,
+    fileUrl: string,
+    disableNotification = false
+  ) => {
+    return axios.post(
+      `${this.apiUrl}/sendDocument`,
+      {
+        chat_id: chatId,
+        document: fileUrl,
+        disable_notification: disableNotification,
+      },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+  };
+
+  sendSticker = (
+    chatId: string,
+    sticker: string,
+    disableNotification = false
+  ) => {
+    return axios.post(
+      `${this.apiUrl}/sendSticker`,
+      {
+        chat_id: chatId,
+        sticker,
+        disable_notification: disableNotification,
+      },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+  };
+
+  sendPoll = (
+    chatId: string,
+    question: string,
+    options: string[],
+    disableNotification = false,
+    isAnonymous = false
+  ) => {
+    return axios.post(
+      `${this.apiUrl}/sendPoll`,
+      {
+        chat_id: chatId,
+        question,
+        options,
+        disable_notification: disableNotification,
+        is_anonymous: isAnonymous,
+      },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+  };
+
+  sendDice = (chatId: string, disableNotification = false) => {
+    return axios.post(
+      `${this.apiUrl}/sendDice`,
+      {
+        chat_id: chatId,
+        disable_notification: disableNotification,
+      },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+  };
+
+  editMessage = (chatId: string, messageId: string, text: string) => {
+    return axios.post(
+      `${this.apiUrl}/editMessageText`,
+      {
+        chat_id: chatId,
+        message_id: messageId,
+        text,
+      },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+  };
+
+  deleteMessage = (chatId: string, messageId: string) => {
+    return axios.post(
+      `${this.apiUrl}/deleteMessage`,
+      {
+        chat_id: chatId,
+        message_id: messageId,
       },
       { headers: { 'Content-Type': 'application/json' } }
     );
